7a24d91611d5d1c824f82f33e191c34314c16f9f
Char <73309364+CharGoldenYT@users.noreply.github.com>
2026-05-19 19:36:23 -0400

    custom 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from '$app/state';
+    import FooterClass from "../webpack/footer.svelte";
+</script>
+
+<main>
+    Error!<br>
+    
+    {page.status} {page.error?.message}
+
+    <FooterClass page={"ErrorPage"}/>
+</main>
